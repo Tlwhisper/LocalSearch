e84d68be295b328e0d6263f7a09627395742ff96
Gonzalo Solera <gonzalo1796@gmail.com>
2018-02-15 21:49:54 +0100

    Starting implementation of SimulatedAnnealing

---
+++ SimulatedAnnealing.cpp
@@ -0,0 +1,10 @@
+#include "SimulatedAnnealing.h"
+
+SimulatedAnnealing::SimulatedAnnealing(double temp, double lambda) {
+    this->temp = temp;
+    this->lambda = lambda;
+}
+
+SimulatedAnnealing::get_next_state() {
+    
+}
